ec2e6154bac046dbca9775916b69857021d99875
Johannes Lehmusvaara <johannes.lehmusvaara@gmail.com>
2021-09-11 22:55:03 +0300

    Use fmt/ranges

--- src/util.h
+++ src/util.h
@@ -3,6 +3,7 @@
 #include <fmt/core.h>
 #include <fmt/color.h>
 #include <fmt/format.h>
+#include <fmt/ranges.h>
 #include <vector>
 #include <sstream>
 #include <iomanip>
@@ -87,65 +88,6 @@ inline std::string pretty_timediff(double diff)
 }
 
 }
-
-template<typename T>
-struct fmt::formatter<std::vector<T>>
-{
-
-    constexpr auto parse(format_parse_context& ctx)
-    {
-        return ctx.end();
-    }
-
-    template<typename FormatContext>
-    auto format(const std::vector<T>& vec, FormatContext& ctx)
-    {
-        std::stringstream fmts;
-        fmts << "[";
-        for (std::size_t i = 0; i < vec.size()-1; i++)
-        {
-            const auto& elem = vec[i];
-            fmts << std::setprecision(12) << elem;
-            fmts << ", ";
-        }
-        fmts << vec[vec.size()-1];
-        fmts << "]";
-            
-        return format_to(
-           ctx.out(),
-           "{}",
-           fmts.str()
-        );
-    }
-};
-
-// template<typename K, typename V>
-// struct fmt::formatter<std::map<K,V>>
-// {
-
-//     constexpr auto parse(format_parse_context& ctx)
-//     {
-//         return ctx.end();
-//     }
-
-//     template<typename FormatContext>
-//     auto format(const std::map<K,V>& map, FormatContext& ctx)
-//     {
-//         std::stringstream fmts;
-//         fmts << "{ ";
-//         for (const auto& [key, value] : map)
-//         {
-//             fmts << key << ": " << value << "; ";
-//         }
-//         fmts << "}";
-            
-//         return format_to(
-//            ctx.out(),
-//            "{}",
-//            fmts.str()
-//         );
-//     }
-// };
 
 
 template<typename F, typename S>
